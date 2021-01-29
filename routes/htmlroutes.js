e80a4bcd18432473137b9219865ecf3955646aae
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-29 16:34:48 -0500

    added html routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+// Dependencies
+// =============================================================
+var router = require("express").Router();
+var path = require("path");
+
+// Routes
+// =============================================================
+
+// Basic route that displays exercise.html
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// // Basic route that displays stats.html
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
